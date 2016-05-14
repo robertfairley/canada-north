f8b956e2abce882ea59cabe82e8d67ba01ed7826
robertfairley <robert@robertfairley.com>
2016-05-13 23:55:48 -0400

    Initialize

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name cng2016App.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the cng2016App
+ */
+angular.module('cng2016App')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name cng2016App.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the cng2016App
+ */
+angular.module('cng2016App')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
